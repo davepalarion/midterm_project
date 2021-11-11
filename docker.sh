99bd8131e7be8276f41792e769fc566015a5c0d0
NetAcad DEVASC <changeme@example.com>
2021-11-11 00:14:12 +0000

    add files

---
+++ docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mkdir tempdir
+mkdir tempdir/templates
+
+cp app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+
+
+echo "COPY ./templates /home/devasc/Desktop/app/templates/" >> tempdir/Dockerfile
+echo "COPY app.py /home/devasc/Desktop/app/" >> tempdir/Dockerfile
+
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t app .
+
+docker run -t -d -p 8080:8080 --name samplerunning app
+docker ps -a
